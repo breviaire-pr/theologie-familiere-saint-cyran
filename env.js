003212ac3d106dd6e60d92326edbe768613a4bc0
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2020-05-16 20:45:34 +0200

    Première version.

---
+++ env.js
@@ -0,0 +1,62 @@
+const sites = [
+    {
+        title: 'Bible de Sacy',
+        url: 'https://bible.sacy.be'
+    },
+    {
+        title: 'Heures de Port-Royal',
+        url: 'https://heures.sacy.be'
+    },
+    {
+        title: 'Bible de Royaumont',
+        url: 'https://royaumont.sacy.be'
+    },
+    {
+        title: 'Considérations de Saint-Cyran',
+        url: 'https://considerations.sacy.be'
+    },
+    {
+        title: 'Hiver - Bréviaire Le Tourneux',
+        url: 'https://breviaire.sacy.be/hiver'
+    },
+    {
+        title: 'Théologie Familière de Saint-Cyran',
+        url: 'https://theologie-familiere.sacy.be'
+    },
+    {
+        title: 'Printemps - Bréviaire Le Tourneux',
+        url: 'https://breviaire.sacy.be/printemps'
+    },
+    {
+        title: 'Été - Bréviaire Le Tourneux',
+        url: 'https://breviaire.sacy.be/ete'
+    },
+    {
+        title: 'Automne - Bréviaire Le Tourneux',
+        url: 'https://breviaire.sacy.be/automne'
+    }
+]
+
+const UI_ENV = {
+    title: 'Théologie Familière et autres écrits de Saint-Cyran',
+    canonical: 'https://theologie-familiere.sacy.be',
+    dataBasePath:'',
+    uiPath: 'ui',
+    bookSelectorLabel: 'Part.',
+    chapterSelectorLabel: 'Ch:',
+    sites,
+    links: [
+        {
+            title: 'rapporter une erreur',
+            url: 'https://github.com/breviaire-pr/theologie-familiere-saint-cyran/issues'
+        },
+        {
+            title: 'code source du site web',
+            url: 'https://github.com/breviaire-pr/theologie-familiere-saint-cyran'
+        },
+        {
+            title: 'autres œuvres de Port-Royal',
+            url: 'https://www.sacy.be'
+        }
+    ]
+}
